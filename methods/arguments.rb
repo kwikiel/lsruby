0f51739edb94ac2222428e69b780382fa14b94f0
Kacper Wikieł <>
2019-06-11 16:00:58 +0200

    Ruby methods

---
+++ methods/arguments.rb
@@ -0,0 +1,13 @@
+#
+#
+#
+/*
+It tells me that somebody did:
+
+calculate_product(a)
+
+With just one argument while correct way should be to provide two:
+
+calculate_product(a,b) 
+
+*/
---
+++ methods/greeting.rb
@@ -0,0 +1,5 @@
+def greeting(name)
+  "Hello " + name.to_s + "!"
+end
+
+puts greeting("Kacper")

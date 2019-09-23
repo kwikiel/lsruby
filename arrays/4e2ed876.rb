29fbd59d635046634dff6d0e31fec2905d46a8f6
Kacper Wikieł <k.wikiel@gmail.com>
2019-09-23 03:10:36 +0200

    Flatten the array

---
+++ arrays/4e2ed876.rb
@@ -0,0 +1,10 @@
+# Flatten the array using Array#flatten 
+#
+# Then assign the flattened array to variable named flat_favorites 
+# And print's it's value using #p 
+
+favorites = [['Dave', 7], ['Miranda', 3], ['Jason', 11]]
+
+flat_favorites = favorites.flatten
+
+p flat_favorites 
